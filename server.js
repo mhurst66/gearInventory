1e96a637c312d754cc5f5ca17b099ec1fc817a77
mhurst <matthewhurst1791@gmail.com>
2024-11-26 18:33:09 -0500

    restoring git remote

---
+++ server.js
@@ -0,0 +1,109 @@
+// Require Statements
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path")
+const session = require('express-session')
+const MongoStore = require("connect-mongo")
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+// Import the authController
+const authController = require("./controllers/auth.js");
+// const User = require("../models/user.js")
+const Item = require("./models/item.js")
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+)
+
+app.use(express.static(path.join(__dirname, "public")))
+
+// Routes
+
+// GET HOME PAGE
+app.get("/", async (req, res) => {
+    res.render("index.ejs", {
+        user: req.session.user,
+    })
+})
+
+// auth Route
+app.use("/auth", authController)
+
+// GET ROUTES
+
+// Render full database
+app.get("/items", async (req, res) => {
+  const allItems = await Item.find()
+  res.render("items/index.ejs", { items: allItems, user: req.session.user })
+})
+// items/new.ejs 
+app.get("/items/new", (req, res) => {
+  res.render("items/new.ejs")
+})
+// ANY ID ROUTE NEEDS TO BE PLACED AFTER NEW ROUTES
+// SHOW a specific Item
+app.get("/items/:itemId", async (req, res) => {
+  const foundItem = await Item.findById(req.params.itemId)
+  res.render("items/show.ejs", { item: foundItem, user: req.session.user })
+})
+// EDIT ROUTE
+app.get("/items/:itemID/edit", async (req, res) => {
+  const foundItem = await Item.findById(req.params.itemID)
+  res.render("items/edit.ejs", {
+    item: foundItem,
+  })
+})
+
+// POST ROUTES
+// new item
+app.post("/items", async (req, res) => {
+  await Item.create(req.body)
+  res.redirect("/items")
+})
+
+// DELETE ROUTE
+app.delete("/items/:itemId", async (req, res) => {
+  await Item.findByIdAndDelete(req.params.itemId)
+  res.redirect("/items")
+})
+
+// UPDATE ROUTE
+app.put("/items/:itemId", async (req, res) => {
+  // Update the item in the database
+  await Item.findByIdAndUpdate(req.params.itemId, req.body)
+
+  // Redirect to the item's show page to see the updates
+  res.redirect(`/items/${req.params.itemId}`)
+})
+
+
+// LISTEN FOR THE SERVER
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
